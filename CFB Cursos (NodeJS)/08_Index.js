4ddb7f4f0efe06c958ab587475ef12820164c78a
Santtana_25 <jhonndev25@gmail.com>
2024-06-15 20:51:09 -0300

    Usando o mysql2 e concetando com o DataBase

--- CFB Cursos (NodeJS)/08_Index.js
+++ CFB Cursos (NodeJS)/08_Index.js
@@ -1,3 +1,8 @@
-(async ()=>{
-    const db= require("./db")
-})()
+(async () => {
+    const integraçaoDB = require("./08_IntegraçãoDB")
+    console.log("Obeter todos os Clientes")
+    const clientes = await integraçaoDB.todosUser()
+    console.log(clientes)
+    
+}
+)()
--- CFB Cursos (NodeJS)/08_IntegraçãoDB.js
+++ CFB Cursos (NodeJS)/08_IntegraçãoDB.js
@@ -1,13 +1,21 @@
 const conectar = async () => {
-    if(global.conexao && global.conexao.state != 'disconected'){
+    if (global.conexao && global.conexao.state != 'disconected') {
         return global.conexao
     }
-    const mysql = require('mysql2/promise')
-    const con = mysql.createConnection("mysql://root:.Parede100localhost:3606/Jdv")
-    console.log("Conectou ao Banco");
-    global.conexao=con
+    const mysql = require("mysql2/promise")
+    const con = mysql.createConnection("mysql://root:.Parede100@localhost:3306/Jdv")
+    console.log("Conectou ao Banco")
+    global.conexao = con
     return con
 }
-conectar()
+// Select 
 
-module.exports = {}
+const todosUser = async () => {
+    const con = await conectar()
+    const [linhas] = await con.query("SELECT * FROM users")
+    return await linhas
+}
+
+
+
+module.exports = { todosUser }
